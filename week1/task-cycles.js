9160f3137020f0c46ae85cc2ddb2d9117375b037
Martin Dressler <m.dressler321@gmail.com>
2025-02-26 04:23:47 +0100

    Add files via upload

---
+++ week1/task-cycles.js
@@ -0,0 +1,36 @@
+// 1 =================================
+// Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+// Examples
+// arrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+// arrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+// arrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+// Your code:
+const arrayOfMultiples = (num, length) => {
+    return Array.from({length: length}, (_, i) => (i + 1) * num);
+};
+
+// 2 =================================
+// Change direction of array
+// TIP: Check if there is function which can help you https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+// Example:
+// changeDirection([0, 1, 2, 3]) ➞ [3, 2, 1, 0]
+// changeDirection([]) ➞ []
+// changeDirection([1, 2]) ➞ [2, 1]
+
+// Your code:
+const changeDirection = (array) => {
+    return array.reverse();
+};
+
+// 3 =================================
+// Create function that takes two arrays and return object with two keys - bigger array, sum all numbers
+// Examples
+// biggerArray([1,2,3,4,5], [50,50]) ➞ { array: [50,50], sum: 100 }
+// biggerArray([1,2,3], [2,3,4]) ➞ { array: [2,3,4], sum: 9 }
+
+// Your code:
+const biggerArray = (array1, array2) => {
+    const sum1 = array1.reduce((x, y) => x + y, 0), sum2 = array2.reduce((x, y) => x + y, 0);
+    return sum1 > sum2 ? {array: array1, sum: sum1} : {array: array2, sum: sum2};
+};
---
+++ week1/task-strings.js
@@ -0,0 +1,33 @@
+// Check functions which could help you solve these tasks https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+// 1 =================================
+// Write function to parameterize a string.
+// Example:
+// console.log(parametrize("Javascript is the best"));
+// "javascript-is-the-best"
+
+// Your code:
+const parametrize = (input) => {
+    return input.toLowerCase().replace(/\s/g, "-");
+};
+
+// 2 =================================
+// Write function that takes object and return sentence
+// Example:
+// giveSentenceForPerson({name: "Obi-wan", position: "Jedi", age: 40, location: "Starwars universe"})
+// ➞ Hello Obi-wan from Starwars universe! It is nice to meet you! You look awesome for your 40, young Jedi!
+
+// Your code:
+const giveSentenceForPerson = (obj) => {
+    return `Hello ${obj.name} from ${obj.location}! It is nice to meet you! You look awesome for your ${obj.age}, young ${obj.position}!`;
+};
+
+// 3 =================================
+// Write function that takes string and replace all `a`, `e`, `i`, `o` with *
+// Example:
+// replace("Hello Javascript") ➞ "H*ll* J*v*scr*pt"
+
+// Your code:
+const replace = (input) => {
+    return input.replace(/[aeio]/gi, "*");
+};
